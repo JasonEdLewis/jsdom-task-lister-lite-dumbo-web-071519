b84c37f5016ee3660f3adaf351bd894e6c9af983
jason <jasonedlewis@gmail.com>
2019-08-28 12:54:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const theForm = document.getElementById("create-task-form");
+  const todoUl = document.getElementById('tasks');
+  const destroy=(event)=>{
+      event.target.remove()
+  }
+
+  const addTask=()=>{
+  event.preventDefault();
+  let taskText = document.getElementById("new-task-description");
+  let newli = setUpLis();
+  
+  newli.innerText = taskText.value;
+  taskText.placeholder ;
+  newli.addEventListener('click', destroy)
+  
+  todoUl.append(newli);
+  
+  
+  };
+
+  let setUpLis =()=>{
+    return li = document.createElement('li')
+  }
+
+
+// the event listener👇🏾
+
+theForm.addEventListener("submit",addTask);
+ 
+
+
+
+
+
+
+
+  
 });
+
+
+
